6f02a7800ef9b31f6410c2e35ac0033627a083eb
NOGA MUKTIWATI <80508180+NOGAMUKTIWATI@users.noreply.github.com>
2023-11-09 22:08:24 +0700

    contact

--- src/app/components/home/contact.js
+++ src/app/components/home/contact.js
@@ -2,9 +2,10 @@ export default function contact() {
   return(
   <div className="lg:flex grid grid-cols-1 w-full gap-5 justify-center lg:mt-40 mt-20">
       <div className="p-5 mt-20 max-w-lg space-y-5">
-        <h2 className="text-1xl font-bold tracking-tighter text-primary">CONTACT US</h2>
+        <p className="text-theme-primary text-xs font-bold">CONTACT US</p>
         <h2 className="text-5xl font-bold tracking-tighter">
-            Konsultasi dengan kami GRATIS!
+            Konsultasi dengan kami 
+            <span className="text-theme-primary ml-2">GRATIS!</span>
         </h2>
         <p className="text-sm text-gray-400">
             Kami siap membantu dan menolong bisnis anda,
